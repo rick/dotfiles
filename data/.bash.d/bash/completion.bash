e21972ce4c50efec1897a5141909c20029b71aed
Rick Bradley <rick@rickbradley.com>
2019-10-22 14:16:13 -0500

    enable heroku bash autocompletion

--- data/.bash.d/bash/completion.bash
+++ data/.bash.d/bash/completion.bash
@@ -4,3 +4,8 @@
 #
 # # pasting with tabs doesn't perform completion
 # zstyle ':completion:*' insert-tab pending
+
+# heroku autocomplete setup
+HEROKU_AC_BASH_SETUP_PATH=/Users/rick/Library/Caches/heroku/autocomplete/bash_setup && \
+  test -f $HEROKU_AC_BASH_SETUP_PATH && \
+  source $HEROKU_AC_BASH_SETUP_PATH
