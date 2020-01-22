d95e9df4c6f410ad19030e6c7ae3015cca1a7eda
Rick Bradley <rick@rickbradley.com>
2020-01-22 13:49:37 -0600

    Set up heroku auto-complete

--- data/.bash.d/bash/config.bash
+++ data/.bash.d/bash/config.bash
@@ -4,3 +4,7 @@ SAVEHIST=10000
 
 # YOLO
 export YOLO=always
+
+# Set up heroku autocompletion
+HEROKU_AC_BASH_SETUP_PATH=/Users/rick/Library/Caches/heroku/autocomplete/bash_setup && test -f $HEROKU_AC_BASH_SETUP_PATH && source $HEROKU_AC_BASH_SETUP_PATH;
+
