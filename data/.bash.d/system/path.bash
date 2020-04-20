5342f241c0e4096dbc1494259ad9becbe5ffc59b
Rick Bradley <rick@rickbradley.com>
2020-04-20 11:50:48 -0500

    remove relative binpath

--- data/.bash.d/system/path.bash
+++ data/.bash.d/system/path.bash
@@ -1,2 +1,2 @@
-export PATH="$HOME/bin:./bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
