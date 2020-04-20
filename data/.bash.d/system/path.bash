381a51c63a10d6e4606ffea74944457280e05710
Rick Bradley <rick@rickbradley.com>
2020-04-20 13:11:21 -0500

    Keep /sbin around in PATH

--- data/.bash.d/system/path.bash
+++ data/.bash.d/system/path.bash
@@ -1,2 +1,2 @@
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
