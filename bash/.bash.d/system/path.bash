d9f54bd8830665b0c60ec68d31228eb0f79e7c8e
Rick Bradley <rick@rickbradley.com>
2023-02-26 18:24:52 -0600

    Not sure why, but we need to explicitly specify the --target for stow on newest macos

--- bash/.bash.d/system/path.bash
+++ bash/.bash.d/system/path.bash
@@ -1,2 +1,2 @@
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:/sbin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$BASH/bin:/sbin:$PATH:/opt/homebrew/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
