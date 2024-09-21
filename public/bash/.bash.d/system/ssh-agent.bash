9669f7e29d77f1abede5d1802760e9ddcc94c8bf
Rick Bradley <rick@rickbradley.com>
2024-09-20 20:40:04 -0500

    Revamp ssh agent stuffs

--- public/bash/.bash.d/system/ssh-agent.bash
+++ public/bash/.bash.d/system/ssh-agent.bash
@@ -1,2 +1,7 @@
-eval $(ssh-agent)
-#ssh-add ~/.ssh/id_ed25519
+if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+  eval `ssh-agent`
+  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l >/dev/null || ssh-add
--- public/bash/.bash_profile
+++ public/bash/.bash_profile
@@ -3,3 +3,5 @@ eval "$(flox activate)"
 
 # Added by `rbenv init` on Thu Sep  5 17:45:54 CDT 2024
 eval "$(rbenv init - --no-rehash bash)"
+
+
