2799614013bd2fa24244e42e10b945af74360ce0
Rick Bradley <rick@rickbradley.com>
2025-07-26 12:56:32 -0500

    Add s5 alias for s5cmd
    https://github.com/peak/s5cmd

--- public/personal/.bash.d/other-aliases.bash
+++ public/personal/.bash.d/other-aliases.bash
@@ -2,3 +2,4 @@ alias plex-proxy='sudo ssh -A -L 443:10.253.102.200:443 -L 902:10.253.102.200:90
 alias deemix='docker run -d --name Deemix -v /Volumes/S14TB-1/media/deemix/music/:/downloads -v /Volumes/S14TB-1/media/deemix/config/:/config -e PUID=1000 -e PGID=1000 -e UMASK_SET=022 -e DEEMIX_SINGLE_USER=true -p 6595:6595 registry.gitlab.com/bockiii/deemix-docker'
 alias savegame='cp .env.local ../scratch/stamped/`date +"%Y%m%d%H%M%S"`.env.local'
 alias loadgame='cp ../scratch/stamped/$(ls -tr ../scratch/stamped/ | tail -1) .env.local'
+alias s5='s5cmd --credentials-file ~/.s3cfg-mine'
