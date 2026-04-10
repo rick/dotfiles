8b8cd69e0dc09268462b02ae5216a2b3dae7c71b
Rick Bradley <rick@rickbradley.com>
2026-04-10 08:20:02 -0500

    Make my default SSH agent check more robust
    not sure how this bitrotted (or conversely, 'ever worked' as is the norm).

--- public/bash/.bash.d/system/ssh-agent.bash
+++ public/bash/.bash.d/system/ssh-agent.bash
@@ -1,7 +1,9 @@
-if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+
+ssh-add -l >/dev/null 2>&1
+if [ $? -eq 2 ]; then
   eval `ssh-agent`
   ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
 fi
 
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
 ssh-add -l >/dev/null || ssh-add
