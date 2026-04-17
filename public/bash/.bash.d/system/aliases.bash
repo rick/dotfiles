e4ec3b2d74a66b81701b8874f3f2fc08f5393814
Rick Bradley <rick@rickbradley.com>
2026-04-16 20:50:19 -0500

    Add sudoers'd sd card eject+sort script
    This needs some lift to not require a sudo password to run this 2-liner. The alias makes the sudo call invisible

--- public/bash/.bash.d/system/aliases.bash
+++ public/bash/.bash.d/system/aliases.bash
@@ -6,3 +6,6 @@ alias unfuck-cameras='sudo killall VDCAssistant'
 alias dps="docker ps -a --no-trunc"
 alias kd="docker ps -a | awk '{print \$1}' | sed '1d; s,^\(.*\),docker stop \1; docker rm \1,' | sh -s -x"
 alias dc="docker-compose"
+
+# misc
+alias sd-sort='sudo ~/bin/sd-sort'
